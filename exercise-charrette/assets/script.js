98f15fc86cdef5c04cb95945f633f510178dc976
dasdivya <divyadas.93@gmail.com>
2017-10-05 17:11:33 -0700

    hi

---
+++ exercise-charrette/assets/script.js
@@ -0,0 +1,260 @@
+$(document).ready(function(){
+
+	$('#button-3').hover(function() {
+		$(this).animate({
+			opacity: 0.4,
+			marginLeft: '0.6in',
+			fontSize: '24px',
+			borderWidth: '10px'
+		}, 1000 );
+	});
+
+	$('#button-12').hover(function() {
+		$(this).animate({
+			opacity: 0.8,
+			marginLeft: '5.6in',
+			fontSize: '74px',
+			borderWidth: '233px'
+		}, 1000 );
+	});
+
+	$('#button-21').click('click touchstart', function() {
+		$(this).animate({height: '20px'}, 600);
+		$(this).animate({height: '300'}, 800);
+		$(this).animate({opacity: '1'}, 200);
+		$(this).animate({opacity: '.8'}, 200);
+		$(this).animate({opacity: '1'}, 200);
+		$(this).animate({opacity: '.6'}, 200);
+		$(this).animate({opacity: '1'}, 200);
+		$(this).animate({opacity: '.4'}, 200);
+		$(this).animate({opacity: '1'}, 200);
+		$(this).animate({opacity: '.2'}, 200);
+		$(this).animate({opacity: '1'}, 200);
+		$(this).animate({opacity: '.4'}, 200);
+		$(this).animate({opacity: '1'}, 200);
+		$(this).animate({opacity: '.6'}, 200);
+		$(this).animate({opacity: '1'}, 200);
+		$(this).animate({opacity: '.8'}, 200);
+		$(this).animate({opacity: '.6'}, 200);
+	});
+
+	$('#button-22').on('click', function(){
+    $(this).animate({
+      width: '150px',
+      height: '150px',
+    }, 1000);
+  		$(this).animate({
+    		height: "100px"
+  }, 500);
+});
+
+$('#button-23').hover(function () {
+
+  		$(this).animate({
+    		height: "100px"
+  }, 500);
+	$(this).animate({toggle: '150px', width: '150px'}, 800);
+});
+
+$(document).ready(function() {
+	$('#button-24').click('click touchstart', function() {
+		$(this).animate({toggle: '150px', width: '150px'}, 800);
+
+	});
+
+});
+
+$(document).ready(function() {
+	$('#button-25').click('click touchstart', function() {
+		$(this).animate({left: '170px'}, 1000);
+		$(this).animate({width: '61.5%'}, 1000);
+});
+});
+
+$(document).ready(function() {
+	$('#button-26').click('click touchstart', function() {
+		// $(this).animate({opacity: '.4'}, 600);
+		$('#button-18').animate({height: '1980px'}, 1000);
+		$('#button-17').animate({width: '1980px'}, 1000);
+		$('#button-16').animate({height: '1980px'}, 1000);
+		$('#button-15').animate({width: '1980px'}, 1000);
+	});
+});
+
+$(document).ready(function() {
+	$('#button-27').click('click touchstart', function() {
+		$(this).animate({opacity: '0.2'}, 2000);
+		$(this).animate({height: '20px'}, 600);
+		$(this).animate({height: '300'}, 800);
+});
+	});
+
+	$(document).ready(function() {
+		$('#button-28').click('click touchstart', function() {
+			$(this).animate({opacity: '0'}, 2);
+	});
+		});
+
+		$('#button-29').hover(function() {
+			$(this).animate({
+				opacity: 0.8,
+				marginLeft: '5.6in',
+				fontSize: '74px',
+				borderWidth: '233px'
+			}, 1000 );
+		});
+
+		$('#button-30').hover(function() {
+			$(this).animate({
+				opacity: 0.8,
+				marginRight: '5.6in',
+				fontSize: '14px',
+				borderWidth: '233px'
+			}, 1000 );
+		});
+
+		$('#button-31').hover(function() {
+			$(this).animate({
+				opacity: 0.1,
+				marginTop: '5.6in',
+				fontSize: '74px',
+				borderWidth: '33px'
+			}, 1000 );
+		});
+
+		$('#button-32').hover(function() {
+			$(this).animate({opacity: '1'}, 200);
+		$(this).animate({opacity: '.8'}, 200);
+		$(this).animate({opacity: '1'}, 200);
+		$(this).animate({opacity: '.6'}, 200);
+		$(this).animate({opacity: '1'}, 200);
+		$(this).animate({opacity: '.4'}, 200);
+		$(this).animate({opacity: '1'}, 200);
+		$(this).animate({opacity: '.2'}, 200);
+		$(this).animate({opacity: '1'}, 200);
+		$(this).animate({opacity: '.4'}, 200);
+		$(this).animate({opacity: '1'}, 200);
+		$(this).animate({opacity: '.6'}, 200);
+		$(this).animate({opacity: '1'}, 200);
+		$(this).animate({opacity: '.8'}, 200);
+		$(this).animate({opacity: '.6'}, 200);
+
+		});
+
+		$('#button-33').hover(function() {
+			$(this).animate({height: '20px', width: '20px'}, 200);
+		$(this).animate({height: '300px', width: '23%'}, 200);
+		$(this).animate({height: '20px', width: '20px'}, 200);
+		$(this).animate({height: '300px', width: '23%'}, 200);
+		$(this).animate({height: '20px', width: '20px'}, 200);
+		$(this).animate({height: '300px', width: '23%'}, 200);
+		$(this).animate({height: '20px', width: '20px'}, 200);
+		$(this).animate({height: '300px', width: '23%'}, 200);
+
+		});
+
+		$('#button-34').hover(function() {
+			$(this).animate({height: '660px'}, 1000);
+		$(this).animate({width: '50.5%'}, 1000);
+		});
+
+		$('#button-35').hover(function() {
+			$(this).animate({marginToptop: '60px'}, 800);
+		$(this).animate({marginLeft: '85%'}, 800);
+		$(this).animate({marginTop: '1940px'}, 800);
+		$(this).animate({marginLeft: '26%'}, 800);
+		});
+
+		$('#button-36').hover(function() {
+			$(this).animate({marginTop: '2900px', opacity: '.2'}, 800);
+		});
+
+		$('#button-37').hover(function() {
+			$(this).animate({marginToptop: '6px'}, 800);
+		$(this).animate({marginLeft: '5%'}, 800);
+		$(this).animate({marginTop: '40px'}, 800);
+		$(this).animate({marginLeft: '6%'}, 800);
+		});
+
+		$(document).ready(function() {
+	$('#button-38').click('click touchstart', function() {
+		$(this).animate({opacity: '0'}, 4000);
+		$(this).animate({width: '0%'}, 4000);
+	});
+});
+
+$(document).ready(function() {
+	$('#button-39').click('click touchstart', function() {
+		$(this).css('background-color', 'blue');
+
+	});
+
+});
+
+$(document).ready(function() {
+	$('#button-40').click('click touchstart', function() {
+		$(this).css('background-color', 'blue');
+		$(this).animate({width: '100%'}, 4000);
+	});
+});
+
+$(document).ready(function() {
+	$('#button-41').click('click touchstart', function() {
+		$(this).css('background-color', 'red');
+		$(this).animate({width: '50%'}, 4000);
+		$(this).animate({opacity: '0.5'}, 4000);
+		$(this).animate({height: '50%'}, 4000);
+	});
+});
+
+$(document).ready(function() {
+	$('#button-42').click('click touchstart', function() {
+		$(this).css('color', 'black');
+	});
+});
+
+$(document).ready(function() {
+	$('#button-43').click('click touchstart', function() {
+		$(this).css('color', 'black');
+		$(this).css('font-size', '52px');
+	});
+});
+
+$(document).ready(function() {
+	$('#button-44').click('click touchstart', function() {
+		$(this).css('color', 'green');
+		$(this).css('background-color', 'white');
+		$(this).css('font-size', '52px');
+	});
+});
+
+$(document).ready(function() {
+	$('#button-45').click('click touchstart', function() {
+		$(this).css('border-radius', '0px');
+		$(this).css('font-size', '52px');
+	});
+});
+
+$(document).ready(function() {
+	$('#button-46').click('click touchstart', function() {
+		$(this).css('border-style', 'groove');
+		$(this).css('border-color', 'black');
+		$(this).css('color', 'black');
+		$(this).css('fontSize', '1133px');
+	});
+});
+
+$(document).ready(function() {
+	$('#button-48').click('click touchstart', function() {
+		$(this).css('border-radius', '600px');
+		$(this).css('border-color', 'black');
+		$(this).css('border-width', '500px');
+		$(this).css('border-style', 'solid');
+		$(this).css('color', 'black');
+	});
+});
+
+
+
+
+});
